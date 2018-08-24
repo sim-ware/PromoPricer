acb70bb5eca62a039872ea3d34c0615e53844123
Samir Gossain <samir.gossain@gmail.com>
2018-08-24 17:30:55 +0100

    Scaffold Checkout class

--- lib/db.rb
+++ lib/db.rb
@@ -1,11 +1,5 @@
-# grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
 class Db
   DATA = [{"ProductCode" => 001, "Name" => "Very Cheap Chair", "Price" => 9.25},
           {"ProductCode" => 002, "Name" => "Little table", "Price" => 45.00},
           {"ProductCode" => 003, "Name" => "Funky Light", "Price" => 19.95}]
-
-  def return_token(response)
-    # return response.body[10...-2]
-  end
-
 end
---
+++ spec/checkout_spec.rb
@@ -0,0 +1,16 @@
+require "checkout"
+
+
+describe Checkout do
+  before(:each) do
+    @Checkout = Checkout.new
+  end
+
+  describe ".new" do
+    context "Creates a new Checkout Object" do
+      it "" do
+      
+      end
+    end
+  end
+end
