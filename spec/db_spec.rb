41739c983acc5b272ae0fba57617b27f7760cf12
Samir Gossain <samir.gossain@gmail.com>
2018-08-24 17:28:43 +0100

    'Db' working

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -3,16 +3,23 @@ require "db"
 
 describe Db do
   before(:each) do
-    # @Db = Db.new
+    @Db = Db.new
   end
 
   describe ".new" do
     context "Creates a new Db Object" do
       it "returns List of Hashes Replicating Test Data" do
-        # @authorizer.authenticate(@valid[0], @valid[1])
-        # token = @authorizer.refresh_token
-        # header = @utils.add_token_to_header(token)
-        # expect(header).to include(:content_type)
+        expect(@Db.class::DATA[0]['ProductCode']).to eql(001)
+        expect(@Db.class::DATA[1]['ProductCode']).to eql(002)
+        expect(@Db.class::DATA[2]['ProductCode']).to eql(003)
+
+        expect(@Db.class::DATA[0]['Name']).to eql('Very Cheap Chair')
+        expect(@Db.class::DATA[1]['Name']).to eql('Little table')
+        expect(@Db.class::DATA[2]['Name']).to eql('Funky Light')
+
+        expect(@Db.class::DATA[0]['Price']).to eql(9.25)
+        expect(@Db.class::DATA[1]['Price']).to eql(45.0)
+        expect(@Db.class::DATA[2]['Price']).to eql(19.95)
       end
     end
   end
